6b4c38961159bff52f616236c84f0be62cb9f5c1
Samson Moshe <samson.moshe@gmail.com>
2021-05-09 21:59:34 +0300

    use str as default while serializing

--- dataManager.py
+++ dataManager.py
@@ -3,16 +3,10 @@ import json
 import arrow
 from os import listdir
 
-def ser(o):
-    if isinstance(o, arrow.Arrow):
-        return o.for_json()
-    else:
-        return json.dumps(o.__dict__, default=ser, indent=4)
-
 class DataManager:
     def saveToFile(p: Paste, filename:str):
         f = open("pastes/{}.json".format(filename), 'w')
-        json_data = json.dumps(p.__dict__, default=ser, indent=4)
+        json_data = json.dumps(p.__dict__, default=str, indent=4)
         f.write(json_data)
         f.close()
 
