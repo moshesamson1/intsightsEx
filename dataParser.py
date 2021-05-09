cce1879463ee69a17f3d8936c2019dc1f7218558
Samson Moshe <samson.moshe@gmail.com>
2021-05-09 15:43:14 +0300

    normalize data

--- dataParser.py
+++ dataParser.py
@@ -4,6 +4,8 @@ import arrow
 import dateutil
 
 class DataParser:
+    DEFUALT_STRINGS = ["untitiled, guest, unknown, anonymous"]
+    
     def parse_link(url:str) -> Paste:
         tree = Downloader.downloadPage(url)
         if tree is None: return None
@@ -18,18 +20,27 @@ class DataParser:
             return None
         
         # todo: validate fetched results
-        return Paste(author=username, title=title, content=content, datetime=DataParser.strdate_to_arrow(date), link=url.split('/')[-1])
+        return Paste(author=DataParser.norm_author_title(username), 
+        title=DataParser.norm_author_title(title), 
+        content=DataParser.norm_content(content), 
+        datetime=DataParser.strdate_to_arrow(date), link=url.split('/')[-1])
     
+    def norm_content(content:str):
+        return content.strip()
+
     def strdate_to_arrow(strdate: str) -> arrow.Arrow:
         basic_date = arrow.get(strdate, 'dddd Do [of] MMMM YYYY hh:mm:ss A')
-        basic_date_fix_tz = fix_time_zone(basic_date, strdate.split(' ')[-1])
+        basic_date_fix_tz = DataParser.norm_time_zone(basic_date, strdate.split(' ')[-1])
         normalized_date = basic_date_fix_tz.to('utc')
         return normalized_date
 
-def fix_time_zone(basicdate:arrow.Arrow, tz_string:str=""):
-    # as far as I can see, all times comes with CDT abbrevian for timezone, which can be several timezones
-    # I chose the us central, and used it throughout the process, failing in case a new one is submitted 
-    if tz_string != "CDT":
-        print("ERR: got unknown timezone: ", tz_string)
-        exit(2)
-    return basicdate.replace(tzinfo=dateutil.tz.gettz('US/Central'))
+    def norm_author_title(i:str):
+        return "" if i.lower() in DataParser.DEFUALT_STRINGS else i
+
+    def norm_time_zone(basicdate:arrow.Arrow, tz_string:str=""):
+        # as far as I can see, all times comes with CDT abbrevian for timezone, which can be several timezones
+        # I chose the us central, and used it throughout the process, failing in case a new one is submitted 
+        if tz_string != "CDT":
+            print("ERR: got unknown timezone: ", tz_string)
+            exit(2)
+        return basicdate.replace(tzinfo=dateutil.tz.gettz('US/Central'))
